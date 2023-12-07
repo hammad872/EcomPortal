c40bbbb0664b8bcb2cf77923b169bfc51e27d777
Qasim Ali <62395180+qasim070@users.noreply.github.com>
2023-12-07 13:53:27 +0500

    update dashboard work

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,34 @@
-import { BrowserRouter, Routes , Route } from 'react-router-dom';
+import React, { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
-import { LoginForm } from './components/LoginForm';
-import {SignupForm} from './components/SignupForm';
-
-
+import LoginForm from './components/LoginForm';
+import SignupForm from './components/SignupForm';
+import NavPanel from './components/NavPanel';
+import Dashboard from './components/Dashboard';
 
 function App() {
+  // State to track user login status
+  const [isLoggedIn, setLoggedIn] = useState(true);
+
+  // Function to update login status
+  const handleLogin = () => {
+    setLoggedIn(true);
+  };
+
   return (
     <>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LoginForm/>}/>
-        <Route path="/signup" element={<SignupForm/>}/>
-      </Routes>
-    </BrowserRouter>
-    
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={<LoginForm onLogin={handleLogin} />}
+          />
+          <Route path="/signup" element={<SignupForm />} />
+        </Routes>
+      </BrowserRouter>
 
+      {/* Conditionally render Dashboard if logged in */}
+      {isLoggedIn && <Dashboard />}
     </>
   );
 }
