5fba7ed2130f20ea134fb9c07b947a81e8de92f7
hammad872 <112769220+hammad872@users.noreply.github.com>
2023-12-10 20:34:24 +0500

    portal

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,55 +1,10 @@
 import React, { useState } from "react";
 import Navbar from "./Navbar";
 import { useNavigate } from "react-router-dom";
-import { DataGrid } from "@mui/x-data-grid";
-
-const columnsTab1 = [
-  { field: "id", headerName: "ID", width: 70 },
-  { field: "firstName", headerName: "First name", width: 130 },
-  { field: "lastName", headerName: "Last name", width: 130 },
-  { field: "age", headerName: "Age", type: "number", width: 90 },
-  {
-    field: "fullName",
-    headerName: "Full name",
-    description: "This column has a value getter and is not sortable.",
-    sortable: false,
-    width: 160,
-    valueGetter: (params) =>
-      `${params.row.firstName || ""} ${params.row.lastName || ""}`,
-  },
-];
-
-const columnsTab2 = [
-  { field: "id", headerName: "ID", width: 70 },
-  { field: "firstName", headerName: "First name", width: 130 },
-  { field: "lastName", headerName: "Last name", width: 130 },
-  { field: "age", headerName: "Age", type: "number", width: 90 },
-  {
-    field: "fullName",
-    headerName: "Full name",
-    description: "This column has a value getter and is not sortable.",
-    sortable: false,
-    width: 160,
-    valueGetter: (params) =>
-      `${params.row.firstName || ""} ${params.row.lastName || ""}`,
-  },
-];
-
-const rows = [
-  { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
-  { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
-  { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
-  { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
-  { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
-  { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
-  { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-  { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
-  { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
-];
+import Table from "./Table";
 
 const Dashboard = () => {
   const [divVisible, setDivVisible] = useState(false);
-  const [activeTab, setActiveTab] = useState("tab1"); // Move the state here
 
   const handleAccountCard = () => {
     // Toggle the visibility state
@@ -131,38 +86,7 @@ const Dashboard = () => {
                 </div>
               </div>
             </div>
-            <div className="container">
-              {/* ... (Your existing code) ... */}
-
-              <div>
-                <button onClick={() => setActiveTab("tab1")}>Tab 1</button>
-                <button onClick={() => setActiveTab("tab2")}>Tab 2</button>
-
-                {activeTab === "tab1" && (
-                  <div id="tab1" style={{ height: 400, width: "100%" }}>
-                    <DataGrid
-                      rows={rows}
-                      columns={columnsTab1}
-                      pageSize={5}
-                      rowsPerPageOptions={[5]}
-                      checkboxSelection
-                    />
-                  </div>
-                )}
-
-                {activeTab === "tab2" && (
-                  <div id="tab2" style={{ height: 400, width: "100%" }}>
-                    <DataGrid
-                      rows={rows}
-                      columns={columnsTab2}
-                      pageSize={5}
-                      rowsPerPageOptions={[5]}
-                      checkboxSelection
-                    />
-                  </div>
-                )}
-              </div>
-            </div>
+            <Table />
           </div>
         </div>
       </div>
---
+++ frontend/src/components/Table.jsx
@@ -0,0 +1,166 @@
+import { DataGrid } from "@mui/x-data-grid";
+import React, { useState } from "react";
+
+const columnsTab1 = [
+  { field: "id", headerName: "ID", width: 70 },
+  { field: "firstName", headerName: "First name", width: 130 },
+  { field: "lastName", headerName: "Last name", width: 130 },
+  { field: "age", headerName: "Age", type: "number", width: 90 },
+  {
+    field: "fullName",
+    headerName: "Full name",
+    description: "This column has a value getter and is not sortable.",
+    sortable: false,
+    width: 160,
+    valueGetter: (params) =>
+      `${params.row.firstName || ""} ${params.row.lastName || ""}`,
+  },
+];
+
+const columnsTab2 = [
+  { field: "id", headerName: "ID", width: 70 },
+  { field: "firstName", headerName: "First name", width: 130 },
+  { field: "lastName", headerName: "Last name", width: 130 },
+  { field: "age", headerName: "Age", type: "number", width: 90 },
+  {
+    field: "fullName",
+    headerName: "Full name",
+    description: "This column has a value getter and is not sortable.",
+    sortable: false,
+    width: 160,
+    valueGetter: (params) =>
+      `${params.row.firstName || ""} ${params.row.lastName || ""}`,
+  },
+];
+
+const columnsTab3 = [
+  { field: "id", headerName: "ID", width: 70 },
+  { field: "firstName", headerName: "First name", width: 130 },
+  { field: "lastName", headerName: "Last name", width: 130 },
+  { field: "age", headerName: "Age", type: "number", width: 90 },
+  {
+    field: "fullName",
+    headerName: "Full name",
+    description: "This column has a value getter and is not sortable.",
+    sortable: false,
+    width: 160,
+    valueGetter: (params) =>
+      `${params.row.firstName || ""} ${params.row.lastName || ""}`,
+  },
+];
+
+const rows1 = [
+  { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
+  { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
+  { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
+  { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
+  { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
+  { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
+  { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
+  { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
+  { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+];
+
+const rows2 = [
+  { id: 1, lastName: "Bhupinder", firstName: "Jon", age: 35 },
+  { id: 2, lastName: "Jogi", firstName: "Cersei", age: 42 },
+  { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
+  { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
+  { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
+  { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
+  { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
+  { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
+  { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+];
+
+const rows3 = [
+  { id: 1, lastName: "Bhau", firstName: "Jon", age: 35 },
+  { id: 2, lastName: "Jogi", firstName: "Cersei", age: 42 },
+  { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
+  { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
+  { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
+  { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
+  { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
+  { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
+  { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+];
+
+const Table = () => {
+  const [activeTab, setActiveTab] = useState("tab1"); // Move the state here
+  return (
+    <div className="container">
+      <div className="mt-4">
+        <div className="custom-tabs">
+          <button
+            className="custom-button"
+            onClick={() => setActiveTab("tab1")}
+          >
+            Tab 1
+          </button>
+          <button
+            className="custom-button"
+            onClick={() => setActiveTab("tab2")}
+          >
+            Tab 2
+          </button>
+          <button
+            className="custom-button"
+            onClick={() => setActiveTab("tab3")}
+          >
+            Tab 3
+          </button>
+        </div>
+
+        {activeTab === "tab1" && (
+          <div
+            id="tab1"
+            style={{ height: 400, width: "100%" }}
+            className="mt-4"
+          >
+            <DataGrid
+              rows={rows1}
+              columns={columnsTab1}
+              pageSize={5}
+              rowsPerPageOptions={[5]}
+              checkboxSelection
+            />
+          </div>
+        )}
+
+        {activeTab === "tab2" && (
+          <div
+            id="tab2"
+            style={{ height: 400, width: "100%" }}
+            className="mt-4 "
+          >
+            <DataGrid
+              rows={rows2}
+              columns={columnsTab2}
+              pageSize={5}
+              rowsPerPageOptions={[5]}
+              checkboxSelection
+            />
+          </div>
+        )}
+
+        {activeTab === "tab3" && (
+          <div
+            id="tab3"
+            style={{ height: 400, width: "100%" }}
+            className="mt-4 "
+          >
+            <DataGrid
+              rows={rows3}
+              columns={columnsTab3}
+              pageSize={5}
+              rowsPerPageOptions={[5]}
+              checkboxSelection
+            />
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Table;
