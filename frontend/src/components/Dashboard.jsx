e7add5285b5af71348377683b5fc3d279937af59
hammad872 <hammadsiddiq98@gmail.com>
2024-01-02 12:31:50 +0500

    habiba

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -55,7 +55,7 @@ const Dashboard = () => {
   
   return (
     
-    <>
+<>
       <div className="container">
         <Header />
         <div className="row">
@@ -63,33 +63,40 @@ const Dashboard = () => {
             <Navbar />
           </div>
           <div className="col-lg-10">
-            <div className="row justify-content-center column-gap-sm-3">
-              <div className="col-lg-3  ">
+            <div className="row justify-content-center align-items-center column-gap-sm-3">
+              <div className="col-lg-4  ">
                 <div className="ship_stat p-3 mx-1">
                   <img src="\assets\box.png" alt="" />
                   <h2>{totalParcels.length}</h2>
                   <p>Total Parcels</p>
                 </div>
               </div>
-              <div className="col-lg-3  ">
+              <div className="col-lg-2  ">
                 <div className="ship_stat p-3 mx-1">
                   <img src="\assets\delivery-truck.png" alt="" />
                   <h2>{deliveredParcels.length}</h2>
                   <p>Delivered</p>
                 </div>
               </div>
-              <div className="col-lg-3  ">
+              <div className="col-lg-2  ">
                 <div className="ship_stat p-3 mx-1">
                   <img src="\assets\reload.png" alt="" />
                   <h2>{inTransitParcels.length}</h2>
                   <p>In Transit</p>
                 </div>
               </div>
-              <div className="col-lg-3  ">
+              <div className="col-lg-2  ">
                 <div className="ship_stat p-3 mx-1">
                   <img src="\assets\restart.png" alt="" />
                   <h2>{returnedParcels.length}</h2>
                   <p>Returned</p>
+                </div>
+              </div>
+              <div className="col-lg-2  ">
+                <div className="ship_stat p-3 mx-1">
+                  <img src="\assets\restart.png" alt="" />
+                  <h2>{returnedParcels.length}</h2>
+                  <p>Cancelled</p>
                 </div>
               </div>
             </div>
